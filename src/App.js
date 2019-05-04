8b7977668fc18cb61d302ecd06e555e152f8310b
Thinhtlt <tranlethanhthinh@gmail.com>
2019-05-05 04:42:10 +0700

    Display app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,114 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React, { Component} from "react";
+import "./App.css";
+import TaskForm from './components/TaskForm';
+import Control from './components/Control';
+import TaskList from './components/TaskList';
+
+class App extends Component{
+  constructor(props) {
+    super(props);
+  
+    this.state = {
+      tasks : [],
+      isDisplayForm : false
+    };
+  }
+
+  componentWillMount(){
+    if(localStorage && localStorage.getItem('tasks')){
+      var tasks = JSON.parse(localStorage.getItem('tasks'));
+      this.setState({
+          tasks : tasks
+      });
+    }
+  }
+
+  onGenerateData = ()=>{
+    var tasks = [
+      {
+        id : this.generateID() ,
+        name : 'Học lập trình',
+        status : true
+      },
+      {
+        id : this.generateID()  ,
+        name : 'Đi bơi',
+        status : false
+      },
+      {
+        id : this.generateID(),
+        name : 'Ngủ',
+        status : true
+      },
+
+    ];
+       this.setState({
+           task : tasks
+       });
+       localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
+
+  s4(){
+    return Math.floor((1+Math.random()) * 0x10000).toString(16).substring(1);
+  }
+
+   generateID(){
+     return this.s4() + this.s4() + '-' + this.s4() + this.s4() + '-' + this.s4() + this.s4();
+   }
+   
+   onToggleForm = () => {
+     this.setState({
+         isDisplayForm : !this.state.isDisplayForm
+     });
+   }
+   onCloseForm = () =>{
+     this.setState({
+         isDisplayForm : false
+     });
+   }
+
+  render(){
+    var { tasks , isDisplayForm } = this.state; // var tasks = this.state.tasks
+    var elmTaskForm = isDisplayForm ? <TaskForm onCloseForm={ this.onCloseForm }/> : '';
+    return(
+   <div className="container">
+  <div className="text-center">
+    <h1>Quản Lý Công Việc</h1>
+    <hr />
+  </div>
+  <div className="row">
+    <div className={ isDisplayForm ? 'col-xs-4 col-sm-4 col-md-4 col-lg-4' : '' }>
+      {/*Form*/}
+      { elmTaskForm }
     </div>
-  );
+    <div className={ isDisplayForm ? 'col-xs-8 col-sm-8 col-md-8 col-lg-8' : 'col-xs-12 col-sm-12 col-md-12 col-lg-12' }>
+        <button type="button" className="btn btn-primary" onClick={ this.onToggleForm }>
+          <span className="fa fa-plus mr-5" 
+                  />Thêm Công Việc
+        </button> 
+        <button type="button"
+                className="btn btn-danger ml-5"
+                onClick={ this.onGenerateData }  >
+          Generate Data
+        </button>
+     {/*Form - end*/}
+
+     {/*Serch - sort*/}
+        <Control/>
+     {/*Search - sort end*/}
+      <div className="row mt-15">
+        <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+          <TaskList tasks={ tasks }/>
+        {/*truyền qua props*/}
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+    );
+  }
 }
+
 
 export default App;
---
+++ src/components/TaskList.js
@@ -0,0 +1,49 @@
+
+import React, { Component} from "react";
+import TaskItem from './TaskItem';
+
+
+class TaskList extends Component{
+  render(){
+    var { tasks } = this.props; // var tasks = this.props.tasks // nhận lại props
+    var elmTasks = tasks.map((task,index)=>{
+        return <TaskItem key={ task.id } index={ index } task={ task }/>
+        // truyền props qua TaskItem
+    });
+    return(
+  <table className="table table-bordered table-hover mt-15">
+            <thead>
+              <tr>
+                <th className="text-center">STT</th>
+                <th className="text-center">Tên</th>
+                <th className="text-center">Trạng Thái</th>
+                <th className="text-center">Hành Động</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td />
+                <td>
+                  <input type="text" className="form-control" />
+                </td>
+                <td>
+                  <select className="form-control">
+                    <option value={-1}>Tất Cả</option>
+                    <option value={0}>Ẩn</option>
+                    <option value={1}>Kích Hoạt</option>
+                  </select>
+                </td>
+                <td />
+              </tr>
+
+              { elmTasks }
+              
+
+            </tbody>
+          </table>
+    );
+  }
+}
+
+
+export default TaskList;
